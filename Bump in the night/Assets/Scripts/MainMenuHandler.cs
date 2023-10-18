fe2207e2d2dc28137bd6c93364b21496dd3960ca
tjobe4340 <tjobe4340@gmail.com>
2023-10-17 22:16:07 -0500

    added main menu and base design

--- Bump in the night/Assets/Scripts/MainMenuHandler.cs
+++ Bump in the night/Assets/Scripts/MainMenuHandler.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class MainMenuHandler : MonoBehaviour
 {
     public void PlayGame(){
-        SceneManager.LoadScene("Midterm");
+        SceneManager.LoadScene("First");
 
     }
 
