52dd59fb8ad2c760481764acbc72ea831f765ff5
tjobe4340 <tjobe4340@gmail.com>
2023-11-30 20:51:09 -0600

    renamed particle system rotation

--- Bump in the night/Assets/mouseFollow.cs
+++ Bump in the night/Assets/mouseFollow.cs
@@ -4,27 +4,27 @@ using UnityEngine;
 
 public class ParticleDirectionController : MonoBehaviour
 {
-    public ParticleSystem particleSystem;
+    public ParticleSystem targetParticleSystem;
 
     void Update()
     {
-        if (particleSystem == null)
+        if (targetParticleSystem == null)
         {
-            Debug.LogError("Particle system is not assigned!");
+            Debug.LogError("Target Particle System is not assigned!");
             return;
         }
 
         // Convert the mouse position to world coordinates
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
-        mousePosition.z = particleSystem.transform.position.z; // Ensure we stay in the 2D plane
+        mousePosition.z = targetParticleSystem.transform.position.z; // Ensure we stay in the 2D plane
 
         // Calculate the direction from the particle system to the mouse position
-        Vector3 direction = (mousePosition - particleSystem.transform.position).normalized;
+        Vector3 direction = (mousePosition - targetParticleSystem.transform.position).normalized;
 
         // Calculate the angle between the particle system's forward vector and the direction vector
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90; // Subtract 90 degrees to align with particle system's forward direction
 
         // Set the particle system's rotation
-        particleSystem.transform.rotation = Quaternion.Euler(0, 0, angle);
+        targetParticleSystem.transform.rotation = Quaternion.Euler(0, 0, angle);
     }
 }
